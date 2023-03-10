cb34e50e2dad206bfbf8323e6ba8e5139c1df13d
hmg <hongmg@bsfit.com.cn>
2023-03-10 17:45:20 +0800

    初始化maven

---
+++ lineage-core/src/main/java/cn/com/pa/Test.java
@@ -0,0 +1,11 @@
+package cn.com.pa;
+
+/**
+ * @author hmg
+ * @date 2023/3/10 17:43
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
